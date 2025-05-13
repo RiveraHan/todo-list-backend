cdf294b5ea53dc88cd932761d3b1c6f8ade3e1b8
Hanzell Rivera <hanzellrivera95@gmail.com>
2025-05-12 23:54:21 -0600

    feat: add express-rate-limit and helmet middleware for enhanced security and request limiting; create tsconfig.build.json for build configuration

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -1,14 +1,40 @@
 import "reflect-metadata";
 import "./container";
-import * as functions from "firebase-functions";
+import * as functions from "firebase-functions/v2";
 import express from "express";
 import cors from "cors";
 import {registerRoutes} from "./interfaces/routes";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
-app.use(cors());
+
+app.use(cors({ origin: true }));
+
+app.use(helmet());
+
 app.use(express.json());
 
+// Enable rate limit
+const limiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: true,
+  handler: (_: express.Request, res: express.Response) => {
+    res.status(429).json({
+      statusCode: 429,
+      body: {
+        error: 'TooManyRequests',
+        errorCode: 'TOO_MANY_REQUESTS',
+        errorMessage: 'Too many requests, please try again later.',
+      },
+    });
+  },
+});
+
+app.use(limiter);
+app
 registerRoutes(app);
 
 export {app};
